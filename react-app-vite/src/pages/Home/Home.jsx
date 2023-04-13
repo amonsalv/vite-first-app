13f50c65d3683475f1efcc630976147997bebc8c
anamonsalve <anamonsalve96@gmail.com>
2023-04-13 13:37:31 -0500

    pintamos y traemos la api

--- react-app-vite/src/pages/Home/Home.jsx
+++ react-app-vite/src/pages/Home/Home.jsx
@@ -1,7 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-    return <main>Hola Mundo</main>;
+    const [characterList, setCharacterList] = useState([]) //traemos un const para traer el personaje, traemos la lista de personajes y la funcion que nos permitira actualizarlo/ tambien un useState con un array vacio para manejar la informacion
+
+    useEffect(() => {
+        const list = fetch('https://rickandmortyapi.com/api/character/?page=1') //usamos una promesa, en la que traemos peticiones, traemos la api que vamos a consumir
+            .then((response) => response.json()) //el then hace que reciba una funcion que reciba la respuesta de json
+            .then((data) => setCharacterList(data.results))
+            .catch(console.error);
+
+    }, []); //trae una funcion en la que llamamos otra funcion, puede o no tener dependecias, pero en nuestro casi, si las tiene []
+    return <main>
+
+        <h1 className="title">Este es el taller de React</h1>
+        <p className="text">
+            Para este taller vamos a usar el api de Rick and Morty
+        </p>
+
+
+        <div className="card">
+            <picture className="card___image">
+                <img src='imagen' alt='nombre' />
+            </picture>
+            <h3 className="card__title ">nombre</h3>
+            <p className="card__text">
+                species: <span>especie</span>
+            </p>
+        </div>
+
+
+
+    </main>;
 };
 
 export default Home;
